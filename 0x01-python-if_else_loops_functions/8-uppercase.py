dd4bc95aa105c3eb420a1ec87a03a432dccc9198
rickiewarph <rickiewarph95@gmail.com>
2023-11-29 07:40:22 -0800

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for iterat in str:
+        tmp = interat
+        if ord(tmp) >= 97 and ord(tmp) <= 122:
+            tmp = chr(ord(iterat) - 32)
+        print("{}".format(tmp)), end='')
+    print()
