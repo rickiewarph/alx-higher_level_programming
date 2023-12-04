3b3721e1ead9037748352acce53591612030acca
rickiewarph <rickiewarph95@gmail.com>
2023-12-04 09:21:12 -0800

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,10 +9,10 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int element;
+	int elmnt;
 
 	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
-	for (element = 0; element < Py_SIZE(p); element++)
-		printf("Element %d: %s\n", element, Py_TYPE(PyList_GetItem(p, element))->tp_name);
+	for (elmnt = 0; elmnt < Py_SIZE(p); elmnt++)
+		printf("Element %d: %s\n", elmnt, Py_TYPE(PyList_GetItem(p, elmnt))->tp_name);
 }
