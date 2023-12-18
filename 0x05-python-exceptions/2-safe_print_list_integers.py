623a49ad45e2b329a2df5c769be934ee29a8f7a2
rickiewarph <rickiewarph95@gmail.com>
2023-12-18 02:23:09 -0800

    2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    for q in range(x):
+        try:
+            print("{:d}".format(my_list[q]), end="")
+            counter += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return counter
