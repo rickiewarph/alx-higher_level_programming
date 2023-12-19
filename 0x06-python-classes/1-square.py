9642e7380c05bd0f65e69f32b1e567a45302cf24
rickiewarph <rickiewarph95@gmail.com>
2023-12-19 08:36:00 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """A class defining a square."""
+
+    def __init__(self, q):
+        """A func constructor.
+        Args:
+            q: The len of a square's side.
+        """
+        self.__q = q
