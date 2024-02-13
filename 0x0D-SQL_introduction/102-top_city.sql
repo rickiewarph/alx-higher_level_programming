af24098d72d02785c19ee3521e9750dcff0c6b5c
rickiewarph <rickiewarph95@gmail.com>
2024-02-13 11:34:10 -0800

    102, 103 corrections

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- Script displaying three cities with the highest avg
 -- temp between July and August.
-SELECT `city`, AVG(`value`) AS `avg_tmp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `avg_tmp` DESC
+ORDER BY `avg_temp` DESC
 LIMIT 3;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Script that displays the maximum temp
 -- of each state, ordered by state name.
-SELECT `state`, MAX(`value`) AS `max_tmp`
+SELECT `state`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
