af8a026b5edc265daf42d96d48819aedf44a6580
rickiewarph <rickiewarph95@gmail.com>
2024-02-14 10:59:58 -0800

    advanced tasks

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- sript to list all shows from hbtn_0d_tvshows_rate using the rating
+-- listing all rows of a tble by the sum of a linked row
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC;
