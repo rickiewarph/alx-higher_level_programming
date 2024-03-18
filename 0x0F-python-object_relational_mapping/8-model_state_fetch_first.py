e6772b8e0367cf219837058c071cf913be7956ff
rickiewarph <rickiewarph95@gmail.com>
2024-03-17 19:12:26 -0700

    6,7,8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Starting link class to table in db
+""" prints the first State object  hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
@@ -13,6 +13,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
+    instance = session.query(State).first()
+    if instance is None:
+        print("Nothing")
+    else:
         print(instance.id, instance.name, sep=": ")
-
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
-"""Starting a link class to table in db
-"""
-import sys
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-from sqlalchemy import (create_engine)
+Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
