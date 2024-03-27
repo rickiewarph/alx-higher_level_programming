ee348846135f5e6edbeff6b8abaac55987f189a4
rickiewarph <rickiewarph95@gmail.com>
2024-03-27 05:26:17 -0700

    0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# Check if the URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the URL and get the size of the body in bytes
-size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r')
-
-# Check if size is not empty
-if [ -z "$size" ]; then
-    echo "Error: Unable to get the size of the body."
-    exit 1
-fi
-
-echo "$size"
+# take in a URL, send a request to that URL, and display size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
