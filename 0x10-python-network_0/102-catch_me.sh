081b0a8f10cafb3d8fd61aadc92a33c3654464a3
rickiewarph <rickiewarph95@gmail.com>
2024-03-27 05:54:26 -0700

    advanced

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script to make a request to 0.0.0.0:5000/catch_me that makes the server
+# to respond with a message with You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
