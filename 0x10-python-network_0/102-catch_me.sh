ac4fd23a30744537cade1cf676ed417f1d014db9
rickiewarph <rickiewarph95@gmail.com>
2024-03-27 05:59:28 -0700

    correction

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script to make a request to 0.0.0.0:5000/catch_me that makes the server
-# to respond with a message with You got me!, in the body of the response
+# script to make a request to 0.0.0.0:5000/catch_me that makes the server to respond with a message with You got me!, in the body of the response
 curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
